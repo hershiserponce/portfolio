42e5742ea6c1730c013356f0fbce6f186717498a
Hershiser Ponce <hershiserponce@gmail.com>
2023-09-06 19:46:42 -0600

    Resuelve conflictos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
@@ -14,33 +13,9 @@ function App() {
         <About />
         <Projects />
         <Contact />
-=======
-import { Home } from './components/Home';
-import { About } from './components/About';
-import { Contact } from './components/Contact';
-import { Projects } from './components/Projects';
-import { Sidebar } from './components/Sidebar';
-
-function App() {
- 
-
-  return (
-    <div className="app">
-      <Sidebar/>
-      <main className="content">
-      
-      <Home/>
-      <About/>
-      <Projects/>
-      <Contact/>
->>>>>>> f0353927d2cd85c57768e6d84a77b43baa53add2
       </main>
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> f0353927d2cd85c57768e6d84a77b43baa53add2
